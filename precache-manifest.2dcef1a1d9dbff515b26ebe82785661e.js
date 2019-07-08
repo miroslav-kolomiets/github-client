08891142643fa5444cf69db8df071d1ea967c248
MiroslavKolomiets <kolomiets.miroslav@pdffiller.team>
2019-07-08 15:39:22 +0300

    Updates

---
+++ precache-manifest.2dcef1a1d9dbff515b26ebe82785661e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ab8c5a325799871fde39219b2fae5b",
+    "url": "/github-client/index.html"
+  },
+  {
+    "revision": "f99bd3c8db9580d043ac",
+    "url": "/github-client/static/css/main.6b08b43d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/github-client/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f99bd3c8db9580d043ac",
+    "url": "/github-client/static/js/main.9964801b.chunk.js"
+  },
+  {
+    "revision": "d2361ecc5845b70e405f",
+    "url": "/github-client/static/js/runtime~main.ae002b04.js"
+  }
+]);
